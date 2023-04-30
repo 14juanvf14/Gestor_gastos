7fb9cbe663f59b37a2d158642f3e2a5c88df55b7
Juanvf14 <63973206+juanvf14@users.noreply.github.com>
2023-04-29 22:13:35 -0500

    Documentación de clase Error.java

--- src/main/java/com/gasto/gasto/Modelo/Error.java
+++ src/main/java/com/gasto/gasto/Modelo/Error.java
@@ -1,13 +1,34 @@
 package com.gasto.gasto.Modelo;
 
+
 import lombok.*;
 import org.springframework.http.HttpStatus;
-
+/**
+ * @author Juan Vasquez
+ * @FechaActualización 29/04/2023
+ *  <p>
+ *  Error:
+ *  Representa a los errores generados en la aplicación
+ *  Está acompañada de los siguientes atributos:
+ *  <ul>
+ *  <li>Código de error</li>
+ *  <li>Mensaje de error</li>
+ *  <li>Estado Http de la solicitud donde se produjo el error</li>
+ *  <p>
+ *  La librería lombok permite generar automáticamente los getter, setter
+ *  y constructores de la clase, con las notaciones que le acompañan con @,
+ *  por su parte Builder le permite la construcción de objetos cuando la clase es instanciada.
+ *  <p>
+ *  La librería HttpStatus de SpringFramework permite representar un estado
+ *  Http
+ *
+ */
 @Getter
 @Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+
 
 
 public class Error {
