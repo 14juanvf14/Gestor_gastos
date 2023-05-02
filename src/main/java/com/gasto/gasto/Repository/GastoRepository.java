c6d5b897907ff583b0d07ef1b4521db0c332db1b
Juanvf14 <63973206+juanvf14@users.noreply.github.com>
2023-05-01 22:20:38 -0500

    Documentación de interfaz GastoRepository.java

--- src/main/java/com/gasto/gasto/Repository/GastoRepository.java
+++ src/main/java/com/gasto/gasto/Repository/GastoRepository.java
@@ -9,6 +9,10 @@ import org.springframework.stereotype.Repository;
  *  Este repositorio es una interfaz que extiende todas las operaciones CRUD de
  *  la clase JPA Repository para gastos, lo cual ocurre en la capa de persistencia.
  *
+ *  @author Juan Vasquez
+ *  @since 29/04/2023
+ *  @version 1.0
+ *
  */
 @Repository
 public interface GastoRepository extends JpaRepository<Gasto, Long> {
