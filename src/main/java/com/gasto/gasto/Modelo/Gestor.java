f67f7676d682b55e69ed46531a2833fc37793ce3
Juanvf14 <63973206+juanvf14@users.noreply.github.com>
2023-05-01 21:24:27 -0500

    Documentación de clase Gestor.java

--- src/main/java/com/gasto/gasto/Modelo/Gestor.java
+++ src/main/java/com/gasto/gasto/Modelo/Gestor.java
@@ -9,6 +9,42 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import java.util.List;
 
+/**
+ *  Gestor
+ *  <p>Es una entidad que hace parte del modelo de la aplicación en SpringBoot
+ *  la cual se encarga de representar los gestores del sistema, cuenta con los siguientes atributos:</p>
+ *  <ul>
+ *  <li>id: Identificador del gestor</li>
+ *  <li>nombre: nombre o apodo de gestor</li>
+ *  <li>email: dirección de e-mail del gestor asignada por el admin</li>
+ *  <li>password: contraseña del gestor asignada por el admin</li>
+ *  <li>rol: El rol de gestor podra ser gestor o administrador, lo que permitira acceder a otras crud por rol</li>
+ *  <li>usuario: Relación de uno a muchos que infiere en la gestión de cada gestor</li>
+ *  </ul>
+ *
+ *  <p>
+ *  La librería lombok permite generar automáticamente los getter, setter
+ *  y constructores de la clase, con las notaciones que le acompañan con @,
+ *  por su parte Builder le permite la construcción de objetos cuando la clase es instanciada.
+ *  </p>
+ *  <p>
+ *  La librería de persistence, permite notaciones como @Entity, @Table, @Id, @Column,
+ *  permite representar el modelo en la capa de persistencia.
+ *  </p>
+ *  <p>
+ *  La libreria de validations permite poner restricciones sobre cada atributo, como
+ *  que la un patron, un e-mail o que el dato no sea nulo.
+ *  </p>
+ *  <p>
+ *  La liberia util de java, nos permitira manejar listasd en la relación
+ *  </p>
+ *
+ *
+ *  @author Juan Vasquez
+ *  @version 1.0
+ *  @since 29/04/2023
+ *
+ */
 
 @Getter
 @Setter
