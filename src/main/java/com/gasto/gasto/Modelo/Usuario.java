34749af60e1d8377927367d4e2e33f8888505b2f
Juanvf14 <63973206+juanvf14@users.noreply.github.com>
2023-05-01 22:14:34 -0500

    Documentación de clase Usuario.java

--- src/main/java/com/gasto/gasto/Modelo/Usuario.java
+++ src/main/java/com/gasto/gasto/Modelo/Usuario.java
@@ -9,9 +9,43 @@ import java.util.List;
 
 /**
  *  Usuario
- *  Esta entidad representa la tabla "usuarios" de la crud
+ *  <p>Es una entidad que hace parte del modelo de la aplicación en SpringBoot
+ *  la cual se encarga de representar los usuarios que son gestionados, cuenta con los siguientes atributos:</p>
+ *  <ul>
+ *  <li>id: Identificador del usuario</li>
+ *  <li>nombre: nombre del usuario</li>
+ *  <li>estado: Indica si el usuario es cliente activo, sieno 1 si y 0 no</li>
+ *  <li>email: dirección de e-mail del usuario</li>
+ *  <li>fecha: Corresponde a la fecha de ingreso del usuario en el sistema</li>
+ *  </ul>
+ *
+ *  <p>
+ *  La librería lombok permite generar automáticamente los getter, setter
+ *  y constructores de la clase, con las notaciones que le acompañan con @,
+ *  por su parte Builder le permite la construcción de objetos cuando la clase es instanciada.
+ *  </p>
+ *  <p>
+ *  La librería de persistence, permite notaciones como @Entity, @Table, @Id, @Column,
+ *  permite representar el modelo en la capa de persistencia.
+ *  </p>
+ *  <p>
+ *  La librería de validations permite poner restricciones sobre cada atributo, como que no sea nulo o vacio, que tenga un valor
+ *  minimo o maximo
+ *  </p>
+ *  <p>
+ *  La librería util de java, nos permitira manejar listas en la relación hacia gastos
+ *  </p>
+ *  <p>
+ *  La librería de time, permite asignar un formato de fecha al atributo correspondiente de gastos.
+ *  </p>
+ *
+ *
+ *  @author Juan Vasquez
+ *  @version 1.0
+ *  @since 29/04/2023
  *
  */
+
 @Getter
 @Setter
 @Builder
