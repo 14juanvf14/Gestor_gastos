44b78ea7f986439124ed2611bedbba6fa9f79118
Juanvf14 <63973206+juanvf14@users.noreply.github.com>
2023-05-02 19:24:48 -0500

    Documentación de services

--- src/main/java/com/gasto/gasto/Service/GestorService.java
+++ src/main/java/com/gasto/gasto/Service/GestorService.java
@@ -5,11 +5,58 @@ import com.gasto.gasto.Modelo.Gestor;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ *  gestor service
+ *  Interfaz que define los métodos para el servicio de gestores.
+ * @author Juan Vasquez
+ * @since 29/04/2023
+ * @version 1.0
+ *
+ */
 public interface GestorService {
+    /**
+     * get all
+     * Obtiene una lista de todos los gestores de la base de datos.
+     *
+     * @return {@link List} una lista de gestores
+     * @see List
+     * @see Gestor
+     */
     List<Gestor> getAll();
+    /**
+     * find by id
+     * Obtiene un gestor por su identificador único.
+     *
+     * @param id el identificador del gestor a buscar
+     * @return {@link Optional} Optional que contiene el gestor encontrado, o vacío si no se encuentra el gestor
+     * @see Optional
+     * @see Gestor
+     */
     Optional<Gestor> findById(Long id);
+    /**
+     * saveGestor
+     * Guarda un gestor en la base de datos.
+     *
+     * @param gestor el gestor a guardar
+     * @return {@link Gestor} retorna el gestor guardado
+     * @see Gestor
+     */
     Gestor save(Gestor gestor);
+    /**
+     * delete by id
+     * Elimina un gestor de la base de datos por su identificador único.
+     *
+     * @param id id identificador unico del gestor a eliminar
+     */
     void deleteById(Long id);
+    /**
+     * updategestor
+     * Permite actualizar un gestor de la base de datos.
+     *
+     * @param gestorActualizado ingresa gestor con datos actualizados
+     * @return {@link Gestor} retorna gestor que ha sido actualizado
+     * @see Gestor
+     */
     Gestor updategestor(Gestor gestorActualizado);
 
 
--- src/main/java/com/gasto/gasto/Service/UsuarioService.java
+++ src/main/java/com/gasto/gasto/Service/UsuarioService.java
@@ -5,13 +5,82 @@ import com.gasto.gasto.Modelo.Usuario;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ *  Usuario service
+ *  Esta es una interfaz que manejan los servicios o la
+ *  logica de negocio para la entidad y controlador Usuario
+ *
+ *  <p>
+ *      Usamos instancias de la clase de Usuario.java para objetos que permitan manejarse hacia
+ *      la capa de persistencia
+ *  </p>
+ *  <p>
+ *      La libreria util nos permite manejar listas para algunos metodos de la interfaz y optional que
+ *      retornara los datos si los mismos estan presente desde la capa de persistencia
+ *  </p>
+ *
+ *  @author Juan Vasquez
+ *  @since 29/04/2023
+ *  @version 1.0
+ *
+ */
+
 public interface UsuarioService {
+
+    /**
+     * Guarda un usuario en la base de datos.
+     *
+     * @param usuario el usuario a guardar
+     * @return {@link Usuario} el usuario guardado
+     * @see Usuario
+     */
     Usuario saveUser(Usuario usuario);
+
+
+    /**
+     *  get all users  Obtiene una lista de todos los usuarios de la base de datos.
+     *
+     * @return {@link List} una lista de usuarios
+     * @see List
+     * @see Usuario
+     */
     List<Usuario> getAllUsers();
+    /**
+     * get user by id
+     * Obtiene un usuario por su identificador único.
+     *
+     * @param id id, el identificador del usuario a buscar
+     * @return {@link Optional} que contiene el usuario encontrado, o vacío si no se encuentra el usuario
+     * @see Optional
+     * @see Usuario
+     */
     Optional<Usuario> getUserById(long id);
+    /**
+     * update user
+     * Actualiza un usuario en la base de datos.
+     *
+     * @param usuarioActualizado se brinda el usuario ya actualizado
+     * @return {@link Usuario} retorna el usuario que ha sido actualizado
+     * @see Usuario
+     */
     Usuario updateUser(Usuario usuarioActualizado);
+    /**
+     * eliminar usuario
+     * Elimina un usuario de la base de datos por su identificador único.
+     *
+     * @param id el identificador del usuario a eliminar
+     */
     void eliminarUsuario(long id);
 
+    /**
+     * get user by email
+     * Obtiene un usuario por su dirección de correo electrónico.
+     *
+     * @param email la dirección de correo electrónico del usuario a buscar
+     * @return {@link Optional} que contiene el usuario encontrado, o vacío si no se encuentra el usuario
+     * @see Optional
+     * @see Usuario
+     */
     Optional<Usuario> getUserByEmail(String email);
 
 
