7a87da8f677d3e14028ce2f8a4084df94d501614
Juanvf14 <63973206+juanvf14@users.noreply.github.com>
2023-05-02 19:21:54 -0500

    Documentación de interfaces de repositorio

--- src/main/java/com/gasto/gasto/Repository/GestorRepository.java
+++ src/main/java/com/gasto/gasto/Repository/GestorRepository.java
@@ -3,5 +3,15 @@ package com.gasto.gasto.Repository;
 import com.gasto.gasto.Modelo.Gestor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/**
+ *  Gestor repository
+ *  Este repositorio es una interfaz que extiende todas las operaciones CRUD de
+ *  la clase JPA Repository para gestores, lo cual ocurre en la capa de persistencia.
+ *
+ *  @author Juan Vasquez
+ *  @since 29/04/2023
+ *  @version 1.0
+ *
+ */
 public interface GestorRepository extends JpaRepository<Gestor, Long> {
 }
--- src/main/java/com/gasto/gasto/Repository/UsuarioRepository.java
+++ src/main/java/com/gasto/gasto/Repository/UsuarioRepository.java
@@ -1,12 +1,21 @@
 package com.gasto.gasto.Repository;
 
-import com.gasto.gasto.Modelo.Gasto;
 import com.gasto.gasto.Modelo.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
+
+/**
+ *  Usuario repository
+ *  Este repositorio es una interfaz que extiende todas las operaciones CRUD de
+ *  la clase JPA Repository para los usuarios, lo cual ocurre en la capa de persistencia.
+ *
+ *  @author Juan Vasquez
+ *  @since 29/04/2023
+ *  @version 1.0
+ *
+ */
 
 
 @Repository
