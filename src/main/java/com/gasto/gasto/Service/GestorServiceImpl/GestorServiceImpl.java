7eed0957dcc9f9151d3b7fbc7800ee963e584c29
Juanvf14 <63973206+juanvf14@users.noreply.github.com>
2023-05-03 10:27:56 -0500

    Documentación de services implementation

--- src/main/java/com/gasto/gasto/Service/GestorServiceImpl/GestorServiceImpl.java
+++ src/main/java/com/gasto/gasto/Service/GestorServiceImpl/GestorServiceImpl.java
@@ -9,32 +9,83 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * Implementación de la interfaz {@link GestorService} que proporciona servicios relacionados con los gestores.
+ * @author Juan vasquez
+ * @since 29/04/2023
+ * @version 1.0
+ */
+
 @Service
 public class GestorServiceImpl implements GestorService {
-
+    /**
+     * gestor repository:
+     * Es una inyección de dependencia del repositorio de gestores
+     */
     @Autowired
     private GestorRepository gestorRepository;
 
+
+    /**
+     * get all:
+     * Obtiene una lista con los gestores en la base de datos
+     *
+     * @return {@link List} retorna una lista con los gestores de la base de datos
+     * @see List
+     * @see Gestor
+     */
     @Override
     public List<Gestor> getAll() {
-        return (List<Gestor>) gestorRepository.findAll();
+        return gestorRepository.findAll();
     }
 
+    /**
+     * find by id:
+     * Busca un objeto "Gestor" en la base de datos a partir de su
+     * identificador unico
+     *
+     * @param id identificador del gestor que se desea buscar
+     * @return {@link Optional} Retorna un objeto "Gestor"
+     * @see Optional
+     * @see Gestor
+     */
     @Override
     public Optional<Gestor> findById(Long id) {
         return gestorRepository.findById(id);
     }
 
+    /**
+     * save:
+     * Guarda un objeto "gestor" en la base de datos
+     *
+     * @param gestor gestor que se desea agregar
+     * @return {@link Gestor} retorna el gestor que se agrego
+     * @see Gestor
+     */
     @Override
     public Gestor save(Gestor gestor) {
         return gestorRepository.save(gestor);
     }
 
+    /**
+     * delete by id
+     * Elimina un gestor de la base de datos con su identificador unico
+     *
+     * @param id identificador el gestor a eliminar
+     */
     @Override
     public void deleteById(Long id) {
         gestorRepository.deleteById(id);
     }
 
+    /**
+     * updategestor
+     * Actualiza un objeto "Gestor" en la base de datos
+     *
+     * @param gestorActualizado se ingresa el objeto gestor actualizado
+     * @return {@link Gestor} retorna el objeto gestor ya actualizado
+     * @see Gestor
+     */
     @Override
     public Gestor updategestor(Gestor gestorActualizado) {
         return gestorRepository.save(gestorActualizado);
--- src/main/java/com/gasto/gasto/Service/UsuarioServiceImpl/UsuarioServiceImpl.java
+++ src/main/java/com/gasto/gasto/Service/UsuarioServiceImpl/UsuarioServiceImpl.java
@@ -9,36 +9,98 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * Implementación de la interfaz {@link UsuarioService} que proporciona servicios relacionados con los usuario.
+ * @author Juan vasquez
+ * @since 29/04/2023
+ * @version 1.0
+ */
+
 @Service
 public class UsuarioServiceImpl implements UsuarioService {
 
+    /**
+     * usuario repository:
+     * Es la inyección de dependencias que corresponde al repositorio de Usuarios
+     *
+     */
     @Autowired
     private UsuarioRepository usuarioRepository;
+    /**
+     * save user
+     * Permite guardar un objeto "usuario" en la base de datos
+     *
+     * @param usuario usuario que se desea aguardar
+     * @return {@link Usuario} retorna el usuario que es guardado
+     * @see Usuario
+     */
     @Override
     public Usuario saveUser(Usuario usuario) {
         return usuarioRepository.save(usuario);
     }
 
+    /**
+     * get all users
+     * Obtiene todos los usuarios de la base de datos
+     *
+     * @return {@link List} retorna un listado con los usuarios
+     * @see List
+     * @see Usuario
+     */
     @Override
     public List<Usuario> getAllUsers() {
-        return (List<Usuario>) usuarioRepository.findAll();
+        return usuarioRepository.findAll();
     }
 
+    /**
+     * get user by id
+     * Obtiene un objeto "usuario" por su identificador unico
+     *
+     * @param id identificador el usuario a buscar
+     * @return {@link Optional} retorna el objeto usuario si existe en la base de datos o
+     * un objeto vacio si no existe
+     * @see Optional
+     * @see Usuario
+     */
     @Override
     public Optional<Usuario> getUserById(long id) {
         return usuarioRepository.findById(id);
     }
 
+    /**
+     * update user
+     * Actualiza un usuario en la base de datos
+     *
+     * @param usuarioActualizado objeto usuario actualizado
+     * @return {@link Usuario} el objeto usuario que se actualizo
+     * @see Usuario
+     */
     @Override
     public Usuario updateUser(Usuario usuarioActualizado) {
         return usuarioRepository.save(usuarioActualizado);
     }
 
+    /**
+     * Eliminar usuario
+     * Elimina un usuario de la base de datos por su identificador unico
+     *
+     * @param id identificador del usuario que se desea eliminar
+     */
     @Override
     public void eliminarUsuario(long id) {
         usuarioRepository.deleteById(id);
     }
 
+    /**
+     * get user by email
+     * Obtiene el usuario por su e-mail
+     *
+     * @param email email del usuario a buscar
+     * @return {@link Optional} retorna un objeto usuario si existe,
+     * si no retorna un objeto vacio
+     * @see Optional
+     * @see Usuario
+     */
     @Override
     public Optional<Usuario> getUserByEmail(String email) {
         return usuarioRepository.findByEmail(email);
