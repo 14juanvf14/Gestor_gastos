7ce1d80c883fd3c8bf3aebdda311320bf8a88d5e
Juanvf14 <63973206+juanvf14@users.noreply.github.com>
2023-05-02 19:24:12 -0500

    Documentación de manejo de excepciones

--- src/main/java/com/gasto/gasto/Excepciones/RequestException.java
+++ src/main/java/com/gasto/gasto/Excepciones/RequestException.java
@@ -4,16 +4,39 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import org.springframework.http.HttpStatus;
 
+/**
+ * Excepción lanzada cuando una solicitud HTTP no se puede procesar correctamente.
+ * Esta excepción contiene un código de error, un estado HTTP y un mensaje descriptivo.
+ * @author Juan vasquez
+ * @version 1.0
+ * @since 29/04/2023
+ */
 @EqualsAndHashCode(callSuper = true)
 @Data
 public class RequestException extends RuntimeException{
+
+    /**
+     * Código de error de la excepción.
+     */
     private String code;
+
+    /**
+     * Estado HTTP que se devuelve al cliente.
+     */
     private HttpStatus status;
 
+
+    /**
+     * Crea una nueva excepción de solicitud con el código de error, el estado HTTP y el mensaje especificados.
+     *
+     * @param code    el código de error de la excepción
+     * @param status  el estado HTTP que se devuelve al cliente
+     * @param mensaje el mensaje descriptivo de la excepción
+     */
     public RequestException(String code, HttpStatus status, String mensaje){
         super(mensaje);
         this.code = code;
         this.status = status;
 
-    };
+    }
 }
