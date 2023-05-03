2d3571ef98615d6c0e18a759a8fb2ea11a1375fc
Juanvf14 <63973206+juanvf14@users.noreply.github.com>
2023-05-02 20:52:31 -0500

    Documentación de services implementation

--- src/main/java/com/gasto/gasto/Service/GastoServiceImpl/GastoServiceImpl.java
+++ src/main/java/com/gasto/gasto/Service/GastoServiceImpl/GastoServiceImpl.java
@@ -8,33 +8,85 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+/**
+ * Implementación de la interfaz {@link GastoService} que proporciona servicios relacionados con los gastos.
+ * @author Juan vasquez
+ * @since 29/04/2023
+ * @version 1.0
+ */
 
 
 @Service
 public class GastoServiceImpl implements GastoService {
+    /**
+     * gasto repository
+     * Es una inyección de dependencia del repositorio de gastos
+     */
     @Autowired
     private GastoRepository gastoRepository;
 
+    /**
+     * find all
+     * Opera con el repositorio para obtener la lista de gastos en la base
+     * de datos
+     *
+     * @return {@link List} Devuelve una lista con los gastos existentes en la base de datos
+     * @see List
+     * @see Gasto
+     */
     @Override
     public List<Gasto> findAll() {
-        return (List<Gasto>) gastoRepository.findAll();
+        return gastoRepository.findAll();
     }
 
+    /**
+     * find by id
+     * Permite buscar un objeto "gasto" en la capa de persistencia
+     * usando el identificador unico para dicho objeto
+     *
+     * @param id identificador del gasto que se desea buscar
+     * @return {@link Gasto}
+     * @see Gasto
+     */
     @Override
     public Gasto findById(Long id) {
         return gastoRepository.findById(id).orElse(null);
     }
 
+    /**
+     * save
+     * Permite guardar un objeto "gasto" en la base de datos
+     * haciendo uso de save del repositorio
+     *
+     * @param gasto gasto que se desea guardar
+     * @return {@link Gasto} retorna el gasto que se guardo
+     * @see Gasto
+     */
     @Override
     public Gasto save(Gasto gasto) {
         return gastoRepository.save(gasto);
     }
 
+    /**
+     * delete by id:
+     * Permite eliminar ub objeto "gasto" de la base de datos
+     * usando su identificador unico
+     *
+     * @param id identificador del gasto que se desea eliminar
+     */
     @Override
     public void deleteById(Long id) {
         gastoRepository.deleteById(id);
     }
 
+    /**
+     * update gasto
+     * Actualizar un objeto "gasto" en la base de datos
+     *
+     * @param gasto ingresa el gasto actualizado
+     * @return {@link Gasto} retorna el objeto gasto que se actualizo
+     * @see Gasto
+     */
     @Override
     public Gasto updateGasto(Gasto gasto) {
         return gastoRepository.save(gasto);
